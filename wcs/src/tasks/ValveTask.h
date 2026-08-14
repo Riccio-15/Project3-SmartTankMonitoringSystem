6bf9e758c3e8c22ed76b78a86b30c4dd2ed83fc1
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-08-14 16:08:57 +0200

    feat: added servo motor device and library and added task for handling the opening of the valve

---
+++ wcs/src/tasks/ValveTask.h
@@ -0,0 +1,21 @@
+#ifndef __VALVE_TASK__
+#define __VALVE_TASK__
+
+#include "devices/ServoMotor.h"
+#include "kernel/Task.h"
+
+class ValveTask : public Task
+{
+public:
+    ValveTask(ServoMotor* motor);
+
+    void tick();
+
+private:
+    ServoMotor* motor;
+    enum ValveState { IDLE, MOVING } state;
+    long startMovementTs;
+    int openingAngle;
+};
+
+#endif

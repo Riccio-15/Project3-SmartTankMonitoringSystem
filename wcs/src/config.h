215903745422adb5d0a9c0613c825f15403ec427
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2026-08-15 15:29:55 +0200

    edit: LCDTask to use dynamic clear, and added useful const inside config file

--- wcs/src/config.h
+++ wcs/src/config.h
@@ -7,4 +7,7 @@ constexpr int BT_PIN = 2;
 constexpr int POT_PIN = A0;
 constexpr int SERVO_PIN = 9;
 
+constexpr int LCD_COLS = 16;
+constexpr int SERIAL_INPUT_BUFFER_SIZE = 32;
+
 #endif
--- wcs/src/tasks/LCDTask.cpp
+++ wcs/src/tasks/LCDTask.cpp
@@ -1,30 +1,36 @@
 #include "tasks/LCDTask.h"
-#include <Arduino.h>
 #include "model/Context.h"
+#include "config.h"
+#include <Arduino.h>
 
-LCDTask::LCDTask(LiquidCrystal_I2C* lcd) : lcd(lcd) {
-
-}
+LCDTask::LCDTask(LiquidCrystal_I2C *lcd) : lcd(lcd) {}
 
 void LCDTask::tick() {
-    String state = "";
-    String opening = "OPENING: " + String(Context::getValveTargetOpen()) + "%";
-    if (!Context::isNetworkOk()) state = "UNCONNECTED";
-    else {
-        if (Context::isAutoMode()) state = "AUTOMATIC";
-        else if(Context::isManualMode()) state = "MANUAL";
-    }
+	String state = "";
+	String opening = "OPENING: " + String(Context::getValveTargetOpen()) + "%";
+	if (!Context::isNetworkOk())
+		state = "UNCONNECTED";
+	else {
+		if (Context::isAutoMode())
+			state = "AUTOMATIC";
+		else if (Context::isManualMode())
+			state = "MANUAL";
+	}
 
-    if (state == lastStateDisplayed && opening == lastOpeningDisplayed) {
-        return; 
-    } else {
-        lcd->clear();
-    }
+	while (opening.length() < LCD_COLS)
+		opening += ' ';
+	while (state.length() < LCD_COLS)
+		state += ' ';
 
-    lcd->setCursor(0, 0);
-    lcd->print(opening);
-    lcd->setCursor(0, 1);
-    lcd->print(state);
-    lastStateDisplayed = state;
-    lastOpeningDisplayed = opening;
+	if (opening != lastOpeningDisplayed) {
+		lcd->setCursor(0, 0);
+		lcd->print(opening);
+		lastOpeningDisplayed = opening;
+	}
+
+	if (state != lastStateDisplayed) {
+		lcd->setCursor(0, 1);
+		lcd->print(state);
+		lastStateDisplayed = state;
+	}
 }

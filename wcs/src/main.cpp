10833b81a193a0bb942c688ce83583230ee4e242
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2026-08-15 14:55:33 +0200

    edit: main to use Scheduler and HWPlatform

--- wcs/src/main.cpp
+++ wcs/src/main.cpp
@@ -1,18 +1,47 @@
+#include "config.h"
+#include "kernel/Protocol.h"
+#include "kernel/Scheduler.h"
+#include "model/HWPlatform.h"
+#include "tasks/LCDTask.h"
+#include "tasks/ModalityTask.h"
+#include "tasks/PotentiometerTask.h"
+#include "tasks/SerialCommTask.h"
+#include "tasks/ValveTask.h"
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+HWPlatform *hw;
+Scheduler *scheduler;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+		Serial.begin(SERIAL_BAUD_RATE);
+
+		hw = new HWPlatform();
+		scheduler = new Scheduler();
+		scheduler->init(50);
+
+		Task *serialCommTask = new SerialCommTask();
+		serialCommTask->init(50);
+		scheduler->addTask(serialCommTask);
+
+		Task *modalityTask = new ModalityTask(hw->getButton());
+		modalityTask->init(50);
+		scheduler->addTask(modalityTask);
+
+		Task *potentiometerTask = new PotentiometerTask(hw->getPotentiometer());
+		potentiometerTask->init(50);
+		scheduler->addTask(potentiometerTask);
+
+		Task *valveTask = new ValveTask(hw->getValveMotor());
+		valveTask->init(50);
+		scheduler->addTask(valveTask);
+
+		Task *lcdTask = new LCDTask(hw->getLCD());
+		lcdTask->init(200);
+		scheduler->addTask(lcdTask);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() { 
+	
+	scheduler->schedule();
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }

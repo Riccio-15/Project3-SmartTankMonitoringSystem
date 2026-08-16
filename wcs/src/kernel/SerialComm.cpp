12bb8862946423f07a42089e040c7127d55df3e3
Martina Malagoli <martina.malagoli4@studio.unibo.it>
2026-08-16 17:43:00 +0200

    fix: Removed issues in sendMode, processMessage and splitMessage

--- wcs/src/kernel/SerialComm.cpp
+++ wcs/src/kernel/SerialComm.cpp
@@ -3,6 +3,8 @@
 #include "model/Context.h"
 #include "config.h"
 
+constexpr int NOT_FOUND = -1;
+
 SerialCommService SerialCommService::instance;
 
 SerialCommService::SerialCommService()
@@ -14,8 +16,10 @@ SerialCommService::SerialCommService()
 void SerialCommService::splitMessage() {
 	String message = instance.inputBuffer;
 	int indexLastCharPrefix = message.indexOf(":");
-	instance.messagePrefix = message.substring(0, indexLastCharPrefix + 1);
-	instance.messageContent = message.substring(indexLastCharPrefix + 1, message.length());
+	if (indexLastCharPrefix != NOT_FOUND) {
+		instance.messagePrefix = message.substring(0, indexLastCharPrefix + 1);
+		instance.messageContent = message.substring(indexLastCharPrefix + 1, message.length());
+	}
 }
 
 void SerialCommService::processMessage() {
@@ -30,6 +34,12 @@ void SerialCommService::processMessage() {
 		Context::setValveTargetOpen(instance.messageContent.toInt());
 		Context::setOpeningTargetJustChanged(true);
 	}
+	else if (instance.messagePrefix.compareTo(NET_PREFIX) == 0) {
+		if (instance.messageContent.compareTo(MSG_NET_LOST) == 0) Context::setNetworkLost();
+		else if (instance.messageContent.compareTo(MSG_NET_OK) == 0) Context::setNetworkOk();
+	}
+	instance.messagePrefix = "";
+	instance.messageContent = "";
 }
 
 void SerialCommService::poll(){
@@ -50,16 +60,18 @@ void SerialCommService::poll(){
 }
 
 void SerialCommService::sendMode(){
-	Serial.print(MODE_PREFIX);
 	if (Context::isNetworkOk()) {
+		Serial.print(MODE_PREFIX);
 		if (Context::isAutoMode()) Serial.print(MSG_MODE_AUTO);
 		else if (Context::isManualMode()) Serial.print(MSG_MODE_MANUAL);
 		Context::setModeJustChanged(false);
+		Serial.print("\n");
 	}
 }
 
 void SerialCommService::sendOpening(){
 	Serial.print(OPEN_PREFIX);
 	Serial.print(Context::getValveTargetOpen());
+	Serial.print("\n");
 	Context::setOpeningTargetJustChanged(false);
 }

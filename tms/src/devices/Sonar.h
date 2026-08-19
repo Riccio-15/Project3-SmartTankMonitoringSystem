61e4cd6233c66fce50027c2ad0006696b93abd02
Filippo Ricciotti <filippo.ricciotti@studio.unibo.it>
2026-08-19 12:12:46 +0200

    add: devices

---
+++ tms/src/devices/Sonar.h
@@ -0,0 +1,24 @@
+#ifndef __SONAR__
+#define __SONAR__
+
+#include "ProximitySensor.h"
+
+#define NO_OBJ_DETECTED -1
+
+class Sonar : public ProximitySensor {
+
+  public:
+    Sonar(int echoPin, int trigPin, long maxTime);
+    float getDistance();
+    void setTemperature(float temp);
+
+  private:
+    float getSoundSpeed();
+
+  private:
+    float temperature;
+    int echoPin, trigPin;
+    long timeOut;
+};
+
+#endif

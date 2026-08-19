e147105359100b25a5bb1e71bbc9c9620af0edef
FireSoul04 <davide.mancini10@studio.unibo.it>
2026-08-19 10:52:24 +0200

    fix: made number floating point because valve task was stopping when step was under 1 during servo motor movement

--- wcs/src/tasks/ValveTask.cpp
+++ wcs/src/tasks/ValveTask.cpp
@@ -1,25 +1,23 @@
 #include "ValveTask.h"
 #include "model/Context.h"
 #include <Arduino.h>
-#include <stdlib.h>
 
 constexpr float ANGLE_TO_PERCENTAGE = 100.0f / 90.0f;
-constexpr int COMPLETE_MOVEMENT_TIME = 10;
-constexpr int MAX_ANGLE_MOTOR = 90;
+constexpr float COMPLETE_MOVEMENT_TIME = 10.0;
+constexpr float MAX_ANGLE_MOTOR = 90.0;
 
 ValveTask::ValveTask(ServoMotor* motor)
-    : motor(motor), state(IDLE), openingAngle(0)
+    : motor(motor), state(IDLE), openingAngle(0.0f)
 {
     motor->on();
 }
 
 void ValveTask::tick()
 {
-
     float delta = (Context::getValveTargetOpen() - Context::getValveCurrentOpen()) / 100.0f;
     switch (state) {
     case IDLE:
-        if (abs(delta) > 0.001f)
+        if (fabs(delta) > 0.001f)
         {
             state = MOVING;
         }
@@ -27,8 +25,9 @@ void ValveTask::tick()
     case MOVING:
         openingAngle += (delta / (float)COMPLETE_MOVEMENT_TIME) * MAX_ANGLE_MOTOR;
         motor->setPosition(openingAngle);
+        Serial.println((delta / (float)COMPLETE_MOVEMENT_TIME) * MAX_ANGLE_MOTOR);
         Context::setValveCurrentOpen(openingAngle * ANGLE_TO_PERCENTAGE);
-        if (abs(delta) < 0.001f)
+        if (fabs(delta) < 0.001f)
         {
             state = IDLE;
         }
